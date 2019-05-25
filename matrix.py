43bcca63e1c8eef2cb2f8f2a64337eaf7e426698
Roman Czerwinski <romanczerwinski@Romans-MacBook-Pro.local>
2019-05-25 15:53:25 -0500

    completed the solve for the easy matrix using simple update and scan, need to implement the slice technique for box sub groups

--- matrix.py
+++ matrix.py
@@ -1,6 +1,33 @@
-import numpy as np
-
-
+# TODO count instances of each possibility within a CellGroup and where
+#       there exist only one instance of said possibility within that group, then: 
+#           instance.set_value(possibility)
+# 
+# TODO use box slices to further remove possibilities from other CellGroups
+#
+# if exists:
+# ++===+===+===++
+# ||   |   |   ||
+# ++---+---+---++
+# ||   |   |   ||
+# ++---+---+---++
+# ||   |   |   ||
+# ++===+===+===++
+# || 1 | 2 | 3 ||
+# ++---+---+---++
+# || 4 | 5 |   || Where 6 and 9 must exist ...
+# ++---+---+---++
+# || 7 | 8 |   || in one of these 2 spaces...
+# ++===+===+===++
+# ||   |   |   ||
+# ++---+---+---++
+# ||   |   |   ||
+# ++---+---+---++
+# ||   |   |   ||
+# ++===+===+===++
+#
+# then 6 & 9 necessarily CANNOT exist in the third column of box 1 above
+# or box 3 below
+# import numpy as np
 EMPTY_GROUP = [None, None, None,
                None, None, None,
                None, None, None]
@@ -48,9 +75,13 @@ COMPLETE_SET = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
 NULL_SET = set([])
 
 
+def list_of_zeroes(set_of_values):
+    return [0 for value in set_of_values]
+      
+
 class NumberSpace:
     def __init__(self):
-        self.possibilities = COMPLETE_SET
+        self.possibilities = COMPLETE_SET.copy()
 
     def add_possibility(self, possibility):
         self.possibilities.add(possibility)
@@ -68,32 +99,45 @@ class CellGroup(NumberSpace):
         self.cells.append(cell)
         if cell.value is not None:
             self.rm_possibility(cell.value)
+        self.refresh_possibilities()
 
     def refresh_possibilities(self):
-        for cell in self.cells:
-            self.rm_possibility(cell.value)
+        self.possibilities = COMPLETE_SET.difference(self.get_values())
 
     def get_values(self):
         return set([cell.value for cell in self.cells if cell.value is not None])
 
+    def scan_instances(self):
+        self.refresh_possibilities()
+        instance_counter = dict(zip(self.possibilities,
+                                    list_of_zeroes(self.possibilities)))
+        for possibility in self.possibilities:
+            for cell in self.cells:
+                if possibility in cell.possibilities:
+                    instance_counter[possibility] += 1
+        values_to_set = [value for value in instance_counter.keys() if instance_counter[value] == 1]
+        cells_solved = len(values_to_set)
+        for value in values_to_set:
+            for cell in self.cells:
+                if value in cell.possibilities:
+                    cell.set_value(value)
+        return cells_solved
+
+
 
 class Cell(NumberSpace):
-    def __init__(self,
-        value,
-        row,
-        column,
-        box):
+    def __init__(self, value, row, column, box):
         NumberSpace.__init__(self)
         self.value = value
         self.row = row
         self.column = column
         self.box = box
         self.is_solved = True if value is not None else False
-        if self.value is not None:
+        if self.is_solved:
             self.row.rm_possibility(self.value)
             self.column.rm_possibility(self.value)
             self.box.rm_possibility(self.value)
-            self.possibilities = NULL_SET
+            self.possibilities = NULL_SET.copy()
     
     def __str__(self):
         return str(self.value)
@@ -106,8 +150,18 @@ class Cell(NumberSpace):
     
     def set_value(self, value):
         self.value = value
-        self.possibilities = NULL_SET
+        self.possibilities = NULL_SET.copy()
         self.is_solved = True
+    
+    def refresh_possibilities(self):
+        if not self.is_solved:
+            self.possibilities = self.row.possibilities.intersection(
+                self.column.possibilities.intersection(
+                    self.box.possibilities))
+            if len(self.possibilities) == 1:
+                self.set_value(self.possibilities.pop())
+        else:
+            self.possibilities = NULL_SET.copy()
     
 class Row(CellGroup):
     def __init__(self, row_number):
@@ -116,7 +170,7 @@ class Row(CellGroup):
 
     def __str__(self):
         my_str = []
-        line_break = "+---+---+---+---+---+---+---+---+---+"
+        line_break = ''.join(["+---" for i in range(len(self.cells))] + ["+"] )
         my_str.append(line_break + '\n|')
         for cell in self.cells:
             my_str.append(f""" {cell.value if cell.value is not None else ' '} |""")
@@ -128,6 +182,7 @@ class Column(CellGroup):
     def __init__(self, column_number):
         CellGroup.__init__(self)
         self.column_number = column_number
+ 
     def __str__(self):
         my_str = []
         line_break = "+---+\n"
@@ -143,28 +198,33 @@ class Box(CellGroup):
         self.box_number = box_number
         self.rows = [Row(0), Row(1), Row(2)]
         self.columns = [Column(0), Column(1), Column(2)]
+        self.slices = []
+
     def __str__(self):
         my_str = []
         line_break = '+---+---+---+'
         nl = '\n|'
         my_str.append(line_break + nl)
         for cell_number in range(len(self.cells)):
-            my_str.append(f""" {self.cells[cell_number].value if self.cells[cell_number].value is not None else ' '} |""")
+            my_str.append(f" {self.cells[cell_number].value if self.cells[cell_number].value is not None else ' '} |")
             if cell_number in [2, 5]:
                 my_str.append('\n' + line_break + nl)
         my_str.append('\n' + line_break) 
         return ''.join(my_str)
 
-    # @method
     def rm_possibility(self, possibility):
         self.possibilities.discard(possibility)
         [row.rm_possibility(possibility) for row in self.rows]
         [column.rm_possibility(possibility) for column in self.columns]
+    
+    def add_cell(self, cell):
+        self.cells.append(cell)
+        cell_num = len(self.cells) - 1
+        col_num = cell_num % 3
+        row_num = cell_num // 3
+        self.columns[col_num].add_cell(cell)
+        self.rows[row_num].add_cell(cell)
 
-class BoxLine(CellGroup):
-    def __init__(self, box, line):
-        self.box = box
-        self.line = line
 
 class Matrix:
     """
@@ -202,6 +262,7 @@ class Matrix:
                 self.rows[row_num].add_cell(this_cell)
                 self.columns[column_num].add_cell(this_cell)
                 self.boxes[box_num].add_cell(this_cell)
+        self.finds = 0
         self.solved = False
         self.stuck = False
 
@@ -251,12 +312,13 @@ class Matrix:
         instances = len([cell.value for cell in self.cells if cell.value == number_to_check])
         if instances == 9:
             self.numbers.pop(self.numbers.index(number_to_check))
-            return 9
+            return (number_to_check, 9)
         else:
-            return instances
+            return (number_to_check, instances)
     
-    def get_missing(self, list_of_numbers):
-        return [number for number in self.numbers if number not in list_of_numbers]
+    def update_remaining_numbers(self):
+        for number in self.numbers:
+            self.check_number(number)
 
     def is_solved(self):
         if not self.numbers:
@@ -265,19 +327,30 @@ class Matrix:
         else:
             return False
 
-    # def 
-
-    def update_possibilities(self, cell):
-        # TODO update all possibilities
+    def update_possibilities(self):
         for cell in self.cells:
-            # do the thing
-            print(cell)
-        return None
+            cell.refresh_possibilities()
 
     def fill_in_answers(self):
-        # TODO fill in answers after possibilities have been updated
-        return None
+        self.finds = 0
+        for row in self.rows:
+            self.finds += row.scan_instances()
+        for column in self.columns:
+            self.finds += column.scan_instances()
+        for box in self.boxes:
+            self.finds += box.scan_instances()
+        if self.finds == 0:
+            self.stuck = True
 
     def solve(self):
         while not self.solved and not self.stuck:
-            break # TODO actually solve it
+            self.update_possibilities()
+            self.fill_in_answers()
+            self.update_remaining_numbers()
+            self.is_solved()
+        if self.solved:
+            print(f"Solved:\n{self.__str__()}")
+        elif self.stuck:
+            print(f"Stuck:\n{self.__str__()}")
+        else:
+            print(f"???:\n{self.__str__()}")
