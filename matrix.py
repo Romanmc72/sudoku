6fcaab94562e330cd46025935d5f92f589297cf7
Roman Czerwinski <romanczerwinski@Romans-MacBook-Pro.local>
2019-06-09 20:42:47 -0500

    moved the samples to their own file

--- matrix.py
+++ matrix.py
@@ -32,49 +32,7 @@
 # then 6 & 9 necessarily CANNOT exist in the third column of box 1 above
 # or box 3 below
 # import numpy as np
-EMPTY_GROUP = [None, None, None,
-               None, None, None,
-               None, None, None]
-EMPTY_MATRIX = [
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None]]
-EASY_SAMPLE_MATRIX = [
-    [7   , None, None, None, None, None, None, None, 3   ],
-    [None, 6   , 4   , None, None, None, 9   , 8   , None],
-    [None, 9   , None, 7   , None, 3   , None, 1   , None],
-    [None, None, 9   , 3   , None, 4   , 6   , None, None],
-    [None, None, None, None, 9   , None, None, None, None],
-    [None, None, 7   , 6   , None, 2   , 1   , None, None],
-    [None, 8   , None, 4   , None, 7   , None, 3   , None],
-    [None, 1   , 3   , None, None, None, 7   , 6   , None],
-    [6   , None, None, None, None, None, None, None, 4   ]]
-MEDIUM_SAMPLE_MATRIX = [
-    [None, 8   , None, 6   , None, 2   , None, 7   , None],
-    [None, None, 2   , None, 1   , None, 3   , None, None],
-    [None, None, None, 3   , None, 8   , None, None, None],
-    [7   , None, 3   , None, 6   , None, 1   , None, 9   ],
-    [None, None, None, None, None, None, None, None, None],
-    [6   , None, 1   , None, 5   , None, 8   , None, 7   ],
-    [None, None, None, 4   , None, 5   , None, None, None],
-    [None, None, 6   , None, 3   , None, 7   , None, None],
-    [None, 3   , None, 2   , None, 6   , None, 9   , None]]
-HARD_SAMPLE_MATRIX = [
-    [None, None, None, None, None, None, None, None, None],
-    [None, None, 4   , None, None, 6   , None, None, 8   ],
-    [None, None, None, None, 5   , 4   , None, 2   , 9   ],
-    [4   , None, 5   , None, None, 9   , None, 6   , None],
-    [None, None, 3   , None, None, None, 8   , None, None],
-    [None, 2   , None, 8   , None, None, 9   , None, 3   ],
-    [8   , 9   , None, 7   , 3   , None, None, None, None],
-    [6   , None, None, 4   , None, None, 2   , None, None],
-    [None, None, None, None, None, None, None, None, None]]
+
 COMPLETE_SET = set([1, 2, 3, 4, 5, 6, 7, 8, 9])
 NULL_SET = set([])
 
