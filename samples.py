1a288711a8a5d15140a2d60d1117ca9aed02723f
Roman Czerwinski <roman.czerwinski@zoro.com>
2020-01-05 13:30:38 -0600

    added newline after the last bracket for spacing

--- samples.py
+++ samples.py
@@ -12,7 +12,8 @@ EMPTY_MATRIX = [
     [None, None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None, None],
     [None, None, None, None, None, None, None, None, None],
-    [None, None, None, None, None, None, None, None, None]]
+    [None, None, None, None, None, None, None, None, None]
+]
 EASY_SAMPLE_MATRIX = [
     [7   , None, None, None, None, None, None, None, 3   ],
     [None, 6   , 4   , None, None, None, 9   , 8   , None],
@@ -22,7 +23,8 @@ EASY_SAMPLE_MATRIX = [
     [None, None, 7   , 6   , None, 2   , 1   , None, None],
     [None, 8   , None, 4   , None, 7   , None, 3   , None],
     [None, 1   , 3   , None, None, None, 7   , 6   , None],
-    [6   , None, None, None, None, None, None, None, 4   ]]
+    [6   , None, None, None, None, None, None, None, 4   ]
+]
 MEDIUM_SAMPLE_MATRIX = [
     [None, 8   , None, 6   , None, 2   , None, 7   , None],
     [None, None, 2   , None, 1   , None, 3   , None, None],
@@ -32,7 +34,8 @@ MEDIUM_SAMPLE_MATRIX = [
     [6   , None, 1   , None, 5   , None, 8   , None, 7   ],
     [None, None, None, 4   , None, 5   , None, None, None],
     [None, None, 6   , None, 3   , None, 7   , None, None],
-    [None, 3   , None, 2   , None, 6   , None, 9   , None]]
+    [None, 3   , None, 2   , None, 6   , None, 9   , None]
+]
 HARD_SAMPLE_MATRIX = [
     [None, None, None, None, None, None, None, None, None],
     [None, None, 4   , None, None, 6   , None, None, 8   ],
@@ -42,7 +45,8 @@ HARD_SAMPLE_MATRIX = [
     [None, 2   , None, 8   , None, None, 9   , None, 3   ],
     [8   , 9   , None, 7   , 3   , None, None, None, None],
     [6   , None, None, 4   , None, None, 2   , None, None],
-    [None, None, None, None, None, None, None, None, None]]
+    [None, None, None, None, None, None, None, None, None]
+]
 
 EZ = m.Matrix(EASY_SAMPLE_MATRIX)
 MD = m.Matrix(MEDIUM_SAMPLE_MATRIX)
